426802ae094c9472ff87c71b0c55906f41e7ce5f
Dominik Gomez <rtjzlr@gmail.com>
2019-11-20 17:42:21 +0100

    Poprawiony Gauss i Partial Gauss, dalej problem z Full Gauss

--- gauss.py
+++ gauss.py
@@ -12,257 +12,252 @@ T = TypeVar('T')
 
 
 class Matrix:
-    def __init__(self, T, n):
-        self.T = T
-        self.n = n
-        self.A = [[T(self.generate_r()) for i in range(n)] for j in range(n)]
-        self.X = [T(self.generate_r()) for i in range(n)]
-        self.Xprim = [T(0) for i in range(n)]
-        self.B = [self.T(0) for i in range(n)]
-        self.C = [[T(0) for i in range(self.n + 1)] for j in range(self.n + 1)]
+	def __init__(self, T, n):
+		self.T = T
+		self.n = n
+		self.A = [[T(self.generate_r()) for i in range(n)] for j in range(n)]
+		self.X = [T(self.generate_r()) for i in range(n)]
+		self.Xprim = [T(0) for i in range(n)]
+		self.B = [self.T(0) for i in range(n)]
+		self.C_matrix = [[T(0) for i in range(self.n + 1)] for j in range(self.n)]
 
-        self.find_B(True)
+		self.find_B(True)
 
-    def find_B(self, new_mults):
-        n = self.n
-        for i in range(n):
-            for j in range(n):
-                self.B[i] += self.A[i][j] * self.X[j]
+	def find_B(self, new_mults):
+		n = self.n
+		for i in range(n):
+			for j in range(n):
+				self.B[i] += self.A[i][j] * self.X[j]
 
-        self.create_C_matrix(new_mults)
+		self.create_C_matrix(new_mults)
 
-    def convert_types(self, T):
-        n = self.n
-        self.C = [[T(0) for i in range(self.n + 1)] for j in range(self.n + 1)]
-        m = len(self.C[0])
-        newX = []
-        newB = []
+	def convert_types(self, T):
+		n = self.n
+		self.C_matrix = [[T(0) for i in range(self.n)] for j in range(self.n + 1)]
+		m = len(self.C[0])
+		newX = []
+		newB = []
 
-        if isinstance(self.A[0][0], Fraction):
-            for i in range(n):
-                for j in range(n):
-                    self.A[i][j] = self.A[i][j].decimal
-                newX.append(self.X[i].decimal)
-                newB.append(self.B[i].decimal)
-        else:
-            for i in range(n):
-                for j in range(n):
-                    self.A[i][j] = T(self.A[i][j])
-                newX.append(T(self.X[i]))
-                newB.append(T(self.B[i]))
-        self.X = newX
-        self.B = newB
-        self.find_B(False)
+		if isinstance(self.A[0][0], Fraction):
+			for i in range(n):
+				for j in range(n):
+					self.A[i][j] = self.A[i][j].decimal
+				newX.append(self.X[i].decimal)
+				newB.append(self.B[i].decimal)
+		else:
+			for i in range(n):
+				for j in range(n):
+					self.A[i][j] = T(self.A[i][j])
+				newX.append(T(self.X[i]))
+				newB.append(T(self.B[i]))
+		self.X = newX
+		self.B = newB
+		self.find_B(False)
 
-    def create_C_matrix(self, new_mults):
-        for i in range(self.n):
-            for j in range(self.n):
-                self.C[i][j] = self.A[i][j]
-        for i in range(self.n):
-            self.C[i][self.n] = self.B[i]
+	def create_C_matrix(self, new_mults):
+		for i in range(self.n):
+			for j in range(self.n):
+				self.C_matrix[i][j] = self.A[i][j]
+		for i in range(self.n):
+			self.C_matrix[i][self.n] = self.B[i]
 
-    def print_A(self):
-        s = "MATRIX A\n"
-        for i in range(self.n):
-            for j in range(self.n):
-                s += str(self.A[i][j]) + "   "
-            s += "\n"
-        print(s)
+	def print_A(self):
+		s = "MATRIX A\n"
+		for i in range(self.n):
+			for j in range(self.n):
+				s += str(self.A[i][j]) + "   "
+			s += "\n"
+		print(s)
 
-    def print_B(self):
-        s = "MATRIX B\n"
-        for i in range(self.n):
-            s += str(self.B[i]) + "\n"
-        print(s)
+	def print_B(self):
+		s = "MATRIX B\n"
+		for i in range(self.n):
+			s += str(self.B[i]) + "\n"
+		print(s)
 
-    def print_C(self):
-        s = "MATRIX C\n"
-        for i in range(self.n):
-            for j in range(self.n + 1):
-                s += str(self.C[i][j]) + "   "
-            s += "\n"
-        print(s)
+	def print_C(self):
+		s = "MATRIX C\n"
+		for i in range(self.n):
+			for j in range(self.n + 1):
+				s += str(self.C_matrix[i][j]) + "   "
+			s += "\n"
+		print(s)
 
-    def print_X(self):
-        s = "MATRIX X\n"
-        for i in range(self.n):
-            s += str(self.X[i]) + "   "
-        s += "\n"
-        print(s)
+	def print_X(self):
+		s = "MATRIX X\n"
+		for i in range(self.n):
+			s += str(self.X[i]) + "   "
+		s += "\n"
+		print(s)
 
-    def print_Xprim(self):
-        s = "MATRIX X\n"
-        for i in range(self.n):
-            s += str(self.Xprim[i]) + "   "
-        s += "\n"
-        print(s)
+	def print_Xprim(self):
+		s = "MATRIX X\n"
+		for i in range(self.n):
+			s += str(self.Xprim[i]) + "   "
+		s += "\n"
+		print(s)
 
-    def print_All(self):
-        self.print_A()
-        self.print_B()
-        self.print_C()
-        self.print_X()
+	def print_All(self):
+		self.print_A()
+		self.print_B()
+		self.print_C()
+		self.print_X()
 
-    def generate_r(self):
-        # return self.T(rnd.randint(-65536, 65535) / 65536)
-        return rnd.randint(1,5)
+	def generate_r(self):
+		# return self.T(rnd.randint(-65536, 65535) / 65536)
+		return rnd.randint(1,5)
 
-    def swap_rows(self, C, current_i, current_j):
-        n = len(C)
-        m = len(C[0])
-        T = self.T
+	def swap_rows(self, C, current_i, current_j):
+		n = len(C)
+		m = len(C[0])
+		T = self.T
 
-        max_index = 0
-        max1 = T(0)
-        found = False
-        for i in range(current_i, n):
-            if T(C[i][current_j]) > max1:
-                max1 = C[i][current_j]
-                max_index = i
-                found = True
-        if found:
-            for j in range(m):
-                temp = C[current_i][j]
-                C[current_i][j] = C[max_index][j]
-                C[max_index][j] = temp
+		max_index = 0
+		max1 = T(0)
+		found = False
+		for i in range(current_i, n):
+			if abs(C[i][current_j]) > abs(max1):
+				max1 = C[i][current_j]
+				max_index = i
+				found = True
+		if found:
+			for j in range(m):
+				temp = C[current_i][j]
+				C[current_i][j] = C[max_index][j]
+				C[max_index][j] = temp
 
-        self.print_C()
-        return C
+		self.print_C()
+		return C
 
-    def swap_all(self, C, current_i, current_j):
-        n = len(C)
-        m = len(C[0])
-        T = self.T
+	def swap_cols(self, C, current_i, current_j):
+		n = len(C)
+		m = len(C[0])
+		T = self.T
 
-        max1 = 0
-        max_row = 0
-        max_col = 0
-        found = False
+		max_index = 0
+		max1 = T(0)
+		found = False
+		for j in range(current_j, m):
+			if abs(C[current_i][j]) > abs(max1):
+				max1 = C[current_i][j]
+				max_index = j
+				found = True
+		if found:
+			for i in range(n):
+				temp = C[i][current_j]
+				C[i][current_j] = C[i][max_index]
+				C[i][max_index] = temp
 
-        for i in range(current_i, n):
-            for j in range(current_j, m):
-                if C[i][j] > max1:
-                    max1 = C[i][j]
-                    max_row = i
-                    max_col = j
-                    found = True
-        if found:
-            for j in range(m):
-                temp = C[current_i][j]
-                C[current_i][j] = C[max_row][j]
-                C[max_row][j] = temp
-            for i in range(n):
-                temp = C[i][current_j]
-                C[i][current_j] = C[i][max_col]
-                C[i][max_col] = temp
+		self.print_C()
+		return C
 
-        self.print_C()
-        return C
+	def swap_all(self, C, current_i, current_j):
+		C = self.swap_rows(C, current_i, current_j)
+		C = self.swap_cols(C, current_i, current_j)
+		return C
 
-    def gauss(self):
-        n = self.n
-        C = self.C
-        Xprim = self.Xprim
-        m = len(C)
+	def reduce(self, C, i):
+		n = len(C)
+		m = len(C[0])
+		first = C[i][i]
+		for k in range(m):
+				C[i][k] = C[i][k] / first
 
-        print("C BEFORE:\n")
-        self.print_C()
+		for j in range(i + 1, n):
+			first = C[j][i]
+			for k in range(m):
+				C[j][k] -= C[i][k] * first
+		return C
 
-        for i in range(n):
-            if C[i][i] == 0:
-                return
-            first = C[i][i]
-            for k in range(m):
-                C[i][k] = C[i][k] / first
+	def gauss(self):
+		n = self.n
+		C = self.C_matrix.copy()
+		Xprim = self.Xprim
+		m = len(C[0])
 
-            for j in range(i + 1, n + 1):
-                for k in range(m):
-                    C[j][k] -= C[i][k] * C[j][i]
+		print("C BEFORE:\n")
+		self.print_C()
 
-        for i in range(n - 1, -1, -1):
-            Xprim[i] = C[i][n] / C[i][i]
-            for k in range(i - 1, -1, -1):
-                C[k][n] -= C[k][i] * Xprim[i]
-        print("C AFTER:\n")
-        self.print_C()
-        self.print_Xprim()
-        return Xprim
+		for i in range(n):
+			if C[i][i] == 0:
+				return
+			C = self.reduce(C, i)
+		for i in range(n - 1, -1, -1):
+			Xprim[i] = C[i][n] / C[i][i]
+			for k in range(i - 1, -1, -1):
+				C[k][n] -= C[k][i] * Xprim[i]
+		print("C AFTER:\n")
+		self.print_C()
+		self.print_Xprim()
+		return Xprim
 
-    def gauss_part(self):
-        n = self.n
-        C = self.C
-        Xprim = self.Xprim
-        m = len(C)
+	def gauss_part(self):
+		n = self.n
+		C = self.C_matrix.copy()
+		Xprim = self.Xprim
+		m = len(C[0])
 
-        print("C BEFORE:\n")
-        self.print_C()
+		print("C BEFORE:\n")
+		self.print_C()
 
-        for i in range(n):
+		for i in range(n):
 
-            C = self.swap_rows(C, i, i) ## dodane
+			C = self.swap_rows(C, i, i)
 
-            if C[i][i] == 0:
-                C = self.swap_rows(C, i, i)
-            first = C[i][i]
-            for k in range(m):
-                C[i][k] /= first
+			if C[i][i] == 0:
+				C = self.swap_rows(C, i, i)
+			C = self.reduce(C, i)
+		for i in range(n - 1, -1, -1):
+			Xprim[i] = C[i][n] / C[i][i]
+			for k in range(i - 1, -1, -1):
+				C[k][n] -= C[k][i] * Xprim[i]
 
-            for j in range(i + 1, n + 1):
-                for k in range(m):
-                    C[j][k] -= C[i][k] * C[j][i]
-            self.print_C()
+		print("C AFTER:\n")
+		self.print_C()
+		self.print_Xprim()
+		return Xprim
 
-        for i in range(n - 1, -1, -1):
-            Xprim[i] = C[i][n] / C[i][i]
-            for k in range(i - 1, -1, -1):
-                C[k][n] -= C[k][i] * Xprim[i]
+	def gauss_full(self):
+		n = self.n
+		C = self.C_matrix.copy()
+		Xprim = self.Xprim
+		m = len(C[0])
 
-        print("C AFTER:\n")
-        self.print_C()
-        self.print_Xprim()
-        return Xprim
+		print("C BEFORE:\n")
+		self.print_C()
 
-    def gauss_full(self):
-        n = self.n
-        C = self.C
-        Xprim = self.Xprim
-        m = len(C)
+		for i in range(n):
 
-        print("C BEFORE:\n")
-        self.print_C()
+			C = self.swap_all(C, i, i) ## dodane
 
-        for i in range(n):
+			if C[i][i] == 0:
+				C = self.swap_all(C, i, i)
 
-            C = self.swap_all(C, i, i) ## dodane
+			C = self.reduce(C, i)
+			self.print_C()
 
-            if C[i][i] == 0:
-                C = self.swap_all(C, i, i)
+		for i in range(n - 1, -1, -1):
+			Xprim[i] = C[i][n] / C[i][i]
+			for k in range(i - 1, -1, -1):
+				C[k][n] -= C[k][i] * Xprim[i]
 
-            first = C[i][i]
-            for k in range(m):
-                C[i][k] /= first
-
-            for j in range(i + 1, n + 1):
-                for k in range(m):
-                    C[j][k] -= C[i][k] * C[j][i]
-            self.print_C()
-
-        for i in range(n - 1, -1, -1):
-            Xprim[i] = C[i][n] / C[i][i]
-            for k in range(i - 1, -1, -1):
-                C[k][n] -= C[k][i] * Xprim[i]
-
-        print("C AFTER:\n")
-        self.print_C()
-        self.print_Xprim()
-        return Xprim
+		print("C AFTER:\n")
+		self.print_C()
+		self.print_Xprim()
+		return Xprim
 
 #MAIN
 
 getcontext().prec = 7
 
 a = Matrix(Decimal, 4)
-a.print_All()
-a.gauss()
-a.gauss_part()
+a.print_X()
+# a.gauss()
+# a.gauss_part()
 a.gauss_full()
+
+# a.print_C()
+# a.swap_all(a.C_matrix, 0, 0)
+# a.swap_all(a.C_matrix, 1, 1)
+# a.swap_all(a.C_matrix, 2, 2)
+# a.swap_all(a.C_matrix, 3, 3)
